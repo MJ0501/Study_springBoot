89b7a0ed3ad749ec17793e4e89d52eeaed76a9b5
MJ0501 <mjay.do@gmail.com>
2024-10-15 13:24:05 +0900

    JPA테스트

---
+++ src/test/java/me/mj/springbootdeveloper/MemberRepositoryTest.java
@@ -0,0 +1,51 @@
+package me.mj.springbootdeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+@DataJpaTest
+class MemberRepositoryTest {
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Sql("/insert-members.sql")
+    @Test
+    void getAllMember(){
+        //when
+        List<Member> members = memberRepository.findAll();
+        //then
+        assertThat(members.size()).isEqualTo(3);
+    }
+    @Sql("/insert-members.sql")
+    @Test
+    void getMemberBy(){
+        //when
+        Member member = memberRepository.findById(2L).get();
+        //then
+        assertThat(member.getName()).isEqualTo("B");
+    }
+    @Sql("/insert-members.sql")
+    @Test
+    void getMemberByName(){
+        //when
+        Member member = memberRepository.findByName("C").get();
+        //then
+        assertThat(member.getId()).isEqualTo(3);
+    }
+    @Test
+    void saveMember(){
+        //given
+        Member member = new Member(1L,"A");
+        //when
+        memberRepository.save(member);
+        //then
+        assertThat(memberRepository.findById(1L).get().getName()).isEqualTo("A");
+    }
+
+}
