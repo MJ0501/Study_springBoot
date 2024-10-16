203443263b7cb82422197de02933f567d5a44deb
MJ0501 <mjay.do@gmail.com>
2024-10-16 14:50:25 +0900

    JUnit마무리

--- src/main/java/me/mj/springbootdeveloper/Member.java
+++ src/main/java/me/mj/springbootdeveloper/Member.java
@@ -7,15 +7,22 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 기본생성자
 @AllArgsConstructor
 @Getter
-@Entity
+@Entity // entity 지정 -> 지정한 게 없으므로 class 이름인 Member테이블과 매칭됨!
 public class Member {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id", updatable = false)
+    @Id // id 를 PK로
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // PK를 자동으로 +1 = AUTO_INCREMENT
+    // 그 외 : AUTO - 선택한 DB dialect에 따라 방식 자동선택(default)
+    //        SEQUENCE - DB SEQUENCE 이용해 PK할당 (주로 ORACLE)
+    //        TABLE - key 생성 TABLE 이용
     private Long id;
-    @Column(name="name", nullable = false)
+    @Column(name="name", nullable = false) // name 은 Not Null
+    // 그 외 : unique - 설정하지 않음 false
+    //        columnDefinition - 컬럼 정보 설정
     private String name;
+    public void changeName(String name){
+        this.name=name;
+    }
 }
--- src/test/java/me/mj/springbootdeveloper/MemberRepositoryTest.java
+++ src/test/java/me/mj/springbootdeveloper/MemberRepositoryTest.java
@@ -1,5 +1,7 @@
 package me.mj.springbootdeveloper;
 
+//import org.hibernate.annotations.processing.SQL;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -13,6 +15,21 @@ import static org.junit.jupiter.api.Assertions.*;
 class MemberRepositoryTest {
     @Autowired
     MemberRepository memberRepository;
+
+//    @Sql("/insert-members.sql")
+//    // @SQL 과는 다르므로 오타 주의하기!!!!
+//    @Test
+//    void deleteAll(){
+//        //when
+//        memberRepository.deleteAll();
+//        //then
+//        assertThat(memberRepository.findAll().size()).isZero();
+//    }
+    // deleteAll()은 Test간의 격리를 보장할 때 쓰임.
+    @AfterEach
+    public void cleanUp(){
+        memberRepository.deleteAll();
+    }
 
     @Sql("/insert-members.sql")
     @Test
@@ -46,6 +63,34 @@ class MemberRepositoryTest {
         memberRepository.save(member);
         //then
         assertThat(memberRepository.findById(1L).get().getName()).isEqualTo("A");
+    }
+    @Test
+    void saveMembers(){
+        //given
+        List<Member> members = List.of(new Member(2L,"B"), new Member(3L,"C"));
+        //when
+        memberRepository.saveAll(members);
+        //then
+        assertThat(memberRepository.findAll().size()).isEqualTo(2);
+    }
+    @Sql("/insert-members.sql")
+    // @SQL 과는 다르므로 오타 주의하기!!!!
+    @Test
+    void deleteMemberById(){
+        //when
+        memberRepository.deleteById(2L);
+        //then
+        assertThat(memberRepository.findById(2L).isEmpty()).isTrue();
+    }
+    @Sql("/insert-members.sql")
+    @Test
+    void update(){
+        //given
+        Member member = memberRepository.findById(2L).get();
+        //when
+        member.changeName("BC");
+        //then
+        assertThat(memberRepository.findById(2L).get().getName()).isEqualTo("BC");
     }
 
 }
